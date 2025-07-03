92a08a03e6f617225b7a721c4d2ec21d9f253925
daistic <matotsuper@gmail.com>
2025-07-03 22:33:18 +0700

    fix: better base enemy behaviour

--- Scripts/zombo.gd
+++ Scripts/zombo.gd
@@ -1,7 +1,23 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-var path: PackedScene
+@export var _move_speed: float = 125.0
+@export var _acceleration_force: float = 100.0
+@export var _jump_power: float = 650.0
+var _move_direction: int = -1
 
-func _ready() -> void:
-	path = GameManager.WALK_PATHS[randi_range(0, GameManager.WALK_PATHS.size())]
-	print(path)
+func _physics_process(_delta: float) -> void:
+	var target_velocity_x = _move_speed * _move_direction
+	var current_velocity_x = linear_velocity.x
+	
+	var force_x = (target_velocity_x - current_velocity_x) * _acceleration_force
+	apply_central_force(Vector2(force_x, 0))
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Wall"):
+		_move_direction = -_move_direction
+	if body.is_in_group("EnemyJump"):
+		jump()
+
+func jump() -> void:
+	apply_central_impulse(Vector2(0, -_jump_power))
+	print ("tried jumping")
