1a58d2cbae6f368aef5956a7f5e29d5aa81799db
daistic <matotsuper@gmail.com>
2025-07-05 12:09:33 +0700

    add: credits

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,4 +1,13 @@
 extends Control
 
+@onready var margin_container: MarginContainer = $MarginContainer
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		margin_container.visible = false
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(GameManager.LEVEL_BASE)
+
+func _on_credits_button_2_pressed() -> void:
+	margin_container.visible = true
